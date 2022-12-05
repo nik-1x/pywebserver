7446b81db9c2ca2f94e5850484fcc2b76f69abfe
nik-1x <67558540+nik-1x@users.noreply.github.com>
2022-12-05 17:07:51 +0500

    new functions

--- app.py
+++ app.py
@@ -1,43 +1,34 @@
 from weblib.server import *
 from weblib.ipmanager import *
 from weblib.utils import *
-import json
+from weblib.tmanager import *
 
 app = App(
     config=Config(
         host=IP('0.0.0.0'),
-        port=Port(8080),
-        debug=False
+        port=Port(8083),
+        debug=True
     ),
     variables={
-        'proj_name': 'Python Web Server Example',
+        'proj_name': 'TUQ API',
         'proj_version': '1.0.0',
-        'proj_author': 'Nick (astralyx.dev)'
+        'proj_author': 'Nick',
+        'host': 'localhost:8083'
     }
 )
 
-@app.router.route('/dump', ['GET'], cache_func=True, cache_size=128)
-async def index(request: dict):
-    return await Render.json(request)
+# host
+@app.router.route('/api/auth', ['POST'], cache_func=True, cache_size=256)
+async def getAds(request: dict):
+    
+    return await Render.json({
+        'request': request
+    })
 
 
-@app.router.route('/api/simpleMethod', ['GET'], cache_func=False)
-async def createInvoice(request):
-    if await RequestEngine().contains(request, ["token", "test_var"]):
-
-        token = request["args"]["token"]
-        test_var = request["args"]["test_var"]
-
-        # do stuff
-
-        return await Render.json({
-            "status": "success"
-        })
-    else:
-        return await Render.json({
-            "error": "Missing arguments"
-        })
+def tasker():
+    print("Working")
 
 
+NewTask(tasker).start()
 app.run(mode="dev")
-
